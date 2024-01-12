b3d94a87aac8a08200a8cc32a89fde6a3f980543
Matias Monteiro <matiasmonteiro15@gmail.com>
2024-01-11 22:30:02 -0300

    Create CRUD Photo

--- api/api/Controllers/PhotoController.cs
+++ api/api/Controllers/PhotoController.cs
@@ -66,5 +66,46 @@ namespace api.Controllers
             var response = _mapper.Map<PhotoResponse>(photo);
             return Ok(response);
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] PhotoRequest request, int id)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                else
+                {
+                    var photo = _service.Find(id);
+                    if(photo == null)
+                    {
+                        return NotFound($"Photo with Id {id} not found");
+                    }
+                    _mapper.Map(request, photo);
+                    _service.Update(photo);
+                    return Ok("Photo updated successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception occurred: {ex.Message}");
+                return StatusCode(500, "An internal error occurred while processing the request.");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var photo = _service.Find(id);
+            if (photo == null)
+            {
+                return NotFound($"Photo with Id {id} not found");
+            }
+
+            _service.Delete(photo);
+            return NoContent();
+        }
     }
 }
--- api/api/Domain/Dto/Response/PhotoResponse.cs
+++ api/api/Domain/Dto/Response/PhotoResponse.cs
@@ -7,9 +7,6 @@ namespace api.Domain.Dto.Response
     {
         public int Id { get; set; }
         public string Url { get; set; }
-
         public int PlaceId { get; set; }
-        [JsonIgnore]
-        public virtual PlaceResponse Place { get; set; }
     }
 }
